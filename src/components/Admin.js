fc5250d930b163f58118d288a954a6b5d75c6fed
bruce-deekay <88024202+bruce-deekay@users.noreply.github.com>
2026-08-25 03:34:24 +0300

    used Navigate component from react-router-dom to help navigate web pages using manual values

---
+++ src/components/Admin.js
@@ -0,0 +1,5 @@
+export const Admin = () => {
+  return (
+    <div className="component">Welcome to the Admin Page</div>
+  )
+}
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,19 @@
+import {useNavigate} from 'react-router-dom';
+
 export const Contact = () => {
+  const navigate = useNavigate();
+  // This holds the logic for the button click event, which will navigate back to the home page when clicked.
+
+  const handleSubmit = () => {
+    console.log('------');
+    navigate('/');
+    // This line uses the navigate function to programmatically change the route to the home page ('/').
+  }
+
   return (
-    <div>Contact</div>
+    <>
+      <div className="component">Contact</div>
+      <button onClick={handleSubmit}>Submit Form</button>
+    </>
   )
 }
